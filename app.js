c77a89bb42b474a79ac1111c6d8591073a887749
daneparke <dapa8522@colorado.edu>
2019-01-26 11:53:39 -0700

    added correct paths for all tables

--- app.js
+++ app.js
@@ -68,6 +68,79 @@ app.put('/hosteluser/:id', (req, res) => {
     queries.editHostelUser(req.params.id, req.body).then(editHostelUser => res.json(editHostelUser))
 })
 
+//Hostel Messages
+app.get('/hostelmessages', (req, res) => {
+    queries.getAllHostelMessages().then(response => res.send(response))
+})
+app.get('/hostelmessages/:id', (req, res) => {
+    queries.getHostelMessageById(req.params.id).then(hostelMessage => res.status(200).send(hostelMessage[0]))
+})
+app.post('/hostelmessages', (req, res) => {
+    queries.createHostelMessage(req.body).then(hostelMessage => {
+        res.send(hostelMessage[0])
+    })
+})
+app.delete('/hostelmessages/:id', (req, res) => {
+    queries.deleteHostelMessage(req.params.id).then(() => res.status(204).send())
+})
+app.put('/hostelmessages/:id', (req, res) => {
+    queries.editHostelMessage(req.params.id, req.body).then(editHostelMessage => res.json(editHostelMessage))
+})
+//User Friends
+app.get('/userfriends', (req, res) => {
+    queries.getAllUserFriends().then(response => res.send(response))
+})
+app.get('/userfriends/:id', (req, res) => {
+    queries.getUserFriendById(req.params.id).then(userFriend => res.status(200).send(userFriend[0]))
+})
+app.post('/userfriends', (req, res) => {
+    queries.createUserFriend(req.body).then(userFriend => {
+        res.send(userFriend[0])
+    })
+})
+app.delete('/userfriends/:id', (req, res) => {
+    queries.deleteUserFriend(req.params.id).then(() => res.status(204).send())
+})
+app.put('/userfriends/:id', (req, res) => {
+    queries.editUserFriend(req.params.id, req.body).then(editUserFriend => res.json(editUserFriend))
+})
+//Friends Messages
+app.get('/friendsmessages', (req, res) => {
+    queries.getAllFriendsMessages().then(response => res.send(response))
+})
+app.get('/friendsmessages/:id', (req, res) => {
+    queries.getFriendsMessageById(req.params.id).then(friendsMessage => res.status(200).send(friendsMessage[0]))
+})
+app.post('/friendsmessages', (req, res) => {
+    queries.createFriendsMessage(req.body).then(friendsMessage => {
+        res.send(friendsMessage[0])
+    })
+})
+app.delete('/friendsmessages/:id', (req, res) => {
+    queries.deleteFriendsMessage(req.params.id).then(() => res.status(204).send())
+})
+app.put('/friendsmessages/:id', (req, res) => {
+    queries.editFriendsMessage(req.params.id, req.body).then(editFriendsMessage => res.json(editFriendsMessage))
+})
+//Trips
+app.get('/trips', (req, res) => {
+    queries.getAllTrips().then(response => res.send(response))
+})
+app.get('/trips/:id', (req, res) => {
+    queries.getTripById(req.params.id).then(trip => res.status(200).send(trip[0]))
+})
+app.post('/trips', (req, res) => {
+    queries.createTrip(req.body).then(trip => {
+        res.send(trip[0])
+    })
+})
+app.delete('/trips/:id', (req, res) => {
+    queries.deleteTrip(req.params.id).then(() => res.status(204).send())
+})
+app.put('/trips/:id', (req, res) => {
+    queries.editTrip(req.params.id, req.body).then(editTrip => res.json(editTrip))
+})
+
 
 // app.delete('/users/:id', (req, res) => {
 //     queries.deleteUser(req.params.id).then(res.status(204).send())
